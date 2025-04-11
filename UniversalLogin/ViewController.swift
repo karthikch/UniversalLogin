101c3076eeb709b3571a0dd1a0e40af8a81e1ba8
karthik ch <karthikkumar410@gmail.com>
2025-04-11 11:34:16 +0530

    Initial commit

---
+++ UniversalLogin/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UniversalLogin
+//
+//  Created by Choppavarapu Karthik Kumar on 10/04/25.
+//
+
+import UIKit
+
+public class ViewController: UIViewController {
+
+     @IBOutlet weak var TextField_password: UITextField!
+     @IBOutlet weak var TextField_username: UITextField!
+    
+    
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func ButtonLogin_Clicked(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+
+
+}
+
